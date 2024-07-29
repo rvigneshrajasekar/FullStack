91e25b2ef28a816ac78a6a74b28597e8c841cbe4
vigneshrajasekar <vigneshrajasekar2110@gmail.com>
2024-07-29 18:01:48 +0530

    main

---
+++ Angular/Day1/MyAngular1/src/app/vignesh/vignesh.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vignesh',
+  templateUrl: './vignesh.component.html',
+  styleUrl: './vignesh.component.css'
+})
+export class VigneshComponent {
+
+}
